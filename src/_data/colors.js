ad2ca68610849e02d8f84a53267580d0f2a49207
nikolai <nikolaisuvanto@gmail.com>
2025-10-21 22:26:51 -0400

    tweak: try catch on color caching

--- src/_data/colors.js
+++ src/_data/colors.js
@@ -2,12 +2,14 @@ const Fetch = require("@11ty/eleventy-fetch");
 
 module.exports = async function () {
   let url = "https://n-s.work/api/colors";
-  return Fetch(url, {
-    requestId: "colorCODES",
+  try {
+    return Fetch(url, {
+      requestId: "colorCODES",
 
-    // Optional: cache duration
-    duration: "10s", // or "1h", "10m", etc.
+      // Optional: cache duration
+      duration: "10s", // or "1h", "10m", etc.
 
-    type: "json",
-  });
+      type: "json",
+    });
+  } catch (err) {}
 };
